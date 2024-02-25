8a7550abc5076ff618fe8017e2ff18f511675e50
Durgesh Chaudhary <48durgesh.kumar@gmail.com>
2024-02-25 23:09:19 +0530

    some reorganizing

--- files/02-variables.cpp
+++ files/02-variables.cpp
@@ -2,6 +2,15 @@
 
 int main()
 {
+  int age;
+  const int weight = 75; // constant variable
+
+  // assign a value later
+  age = 25;
+
+  cout << age << endl;
+  cout << weight << endl;
+
   int a = 10;
   int b = 20;
 
